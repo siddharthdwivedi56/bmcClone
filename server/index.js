b0856f4f02edf5bcefbc7a6b0b985220ac2e62c3
siddharthdwivedi56 <siddharth.d@crownstack.com>
2024-11-30 23:38:52 +0530

    gitignore

--- server/index.js
+++ server/index.js
@@ -4,16 +4,16 @@ const userRoute = require('./routes/userRoute');
 
 const app = express();
 
-/** connecting mongoose with node application */
-const dbUrl = "mongodb+srv://siddharthdwivedi56:CKhkWarlzczxqtfN@cluster0.tt6sv.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0";
+/** Connecting mongoose with node application */
+const dbUrl = "mongodb+srv://siddharthdwivedi56:<password>@cluster0.tt6sv.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(dbUrl).then((connection)=>{
     console.log('db connection done')
 }).catch(err => console.log(err));
 
-/** to collect data as json from api request */
+/** To collect data as json from api request */
 app.use(express.json());
 
-/**List of routes */
+/** List of routes */
 app.use('/api',userRoute);
 
 
