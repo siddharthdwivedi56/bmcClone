835870a92b71c49b494515d2bfa676217ff356dc
siddharthdwivedi56 <siddharth.d@crownstack.com>
2024-12-01 11:08:12 +0530

    added .env file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoute = require('./routes/userRoute');
 
+
 const app = express();
 
 /** Connecting mongoose with node application */
-const dbUrl = "mongodb+srv://siddharthdwivedi56:<password>@cluster0.tt6sv.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0";
+const dbUrl =  process.env.DB_URI;
 mongoose.connect(dbUrl).then((connection)=>{
     console.log('db connection done')
 }).catch(err => console.log(err));
@@ -15,10 +17,4 @@ app.use(express.json());
 
 /** List of routes */
 app.use('/api',userRoute);
-
-
-app.get('/login',(req,res)=>{
-    res.json({"s":123}); 
-})
-
 app.listen(8001);
